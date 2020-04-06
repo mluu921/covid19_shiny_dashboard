56e9f6df196143a93b96fe40901e996a6b62718a
Michael Luu <mluu921@gmail.com>
2020-04-05 19:37:17 -0700

    change over to flex dashboard and googlesheets

--- 00-update_data_to_googlesheets.R
+++ 00-update_data_to_googlesheets.R
@@ -22,5 +22,31 @@ update_lac_dph_data <- function() {
 }
 
 
+update_lac_dph_data_summary <- function() {
+  url <- 'http://publichealth.lacounty.gov/media/Coronavirus/locations.htm'
+  
+  dat <- read_html(url) %>%
+    html_table() %>%
+    .[[1]] %>%
+    as_tibble()
+  
+  data <- tibble(
+    date = Sys.Date(),
+    total_cases = as.numeric(dat[dat$X1 == "Laboratory Confirmed Cases (LCC)",]$X2),
+    total_deaths = as.numeric(dat$X2[[7]])
+  )
+  
+  # sheets_write(data, '1lZ7QJ2jKC0fHh4f3FkyB4WC-7EUNwiBwP9wX9WjTdLw', sheet = 'summary')
+  sheets_append(data, '1lZ7QJ2jKC0fHh4f3FkyB4WC-7EUNwiBwP9wX9WjTdLw', sheet = 'summary')
+  
+}
+
 update_lac_dph_data()
+update_lac_dph_data_summary()
+
+
+
+
+
+
 
